64002cf9b2e1e0f9b27a9f246b24a6405a2a0a82
satyamwathrey <swathrey1@gmail.com>
2022-04-11 10:30:39 +0530

    Tc 1.2- Max Number at second place

--- Program.cs
+++ Program.cs
@@ -14,13 +14,12 @@ namespace TestMaximum
                 //Calling Class to call a Function
                 MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
 
-                //Maximum Number At First Place
-                int outputInteger = maximumNumberCheck.MaximumIngerNumber(411, 282, 333);
+                //Maximum Number At Second Place
+                int outputInteger = maximumNumberCheck.MaximumIngerNumber(41, 82, 33);
 
                 //Display Output
                 Console.WriteLine("Maximum Number of Integer : " + outputInteger);
 
-                Console.ReadLine();
 
             }
         }
