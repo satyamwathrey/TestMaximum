307bfe4c4dcd47205ca1e91d55523e5c790f20d5
satyamwathrey <swathrey1@gmail.com>
2022-04-11 10:22:18 +0530

    Tc 1.1- Max Number at first place

---
+++ MaximumNumberCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    class MaximumNumberCheck
+    {
+        //Function To check Maximum Number
+        public int MaximumIngerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            //Comaring 3 Numbers
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("First, Second and Third number are same : ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                //Welcome Message
+                Console.WriteLine("Welcome to Maximum Number");
+
+                //Calling Class to call a Function
+                MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+
+                //Maximum Number At First Place
+                int outputInteger = maximumNumberCheck.MaximumIngerNumber(411, 282, 333);
+
+                //Display Output
+                Console.WriteLine("Maximum Number of Integer : " + outputInteger);
+
+                Console.ReadLine();
+
+            }
+        }
+    }   
+}   
